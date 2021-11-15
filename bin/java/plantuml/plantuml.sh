debea9ffe5e327ff90718b13a396a8b5205be3db
TaesungKim <taesung.kim@posicube.com>
2021-11-15 19:06:04 +0900

    set plantuml env. add symbolic file for linux

---
+++ bin/java/plantuml/plantuml.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+usage="Usage:\t[CMD] [TARGET_FILE_1] [TARGET_FILE_2] [TARGET_FILE_3] ... [TARGET_FILE_n]"
+
+echo "Numer of arguments : $#"
+
+if		[ $# -lt 1 ]; then
+	echo "$usage"
+	exit 1
+else
+	java -jar ~/DevEnv/bin/java/plantuml.jar -verbose $@
+fi
+
