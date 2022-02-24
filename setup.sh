cd0d486c5def1be3cdcc1bbbe0132705820ac5ab
PleYa <aaiaia.gg@gmail.com>
2022-02-24 02:07:07 -0800

    added setup.sh

--- setup.sh
+++ setup.sh
@@ -1,2 +1,23 @@
-#!/bin/sh
-chmod 700 ~/DevEnv
+#!/bin/bash
+BASIC_ENV=./.setup/env/sh/setup_env
+
+source $BASIC_ENV
+
+CURRENT_DIR=$PWD
+if	[ -e $BASIC_ENV ]; then
+	chmod 700 $ENV_HOME
+	echo "chmod 700 $ENV_HOME"
+
+	# Config vim colors
+	CURRENT_DIR=$PWD
+	echo "Backup loaction: $CURRENT_DIR"
+	cd ./vim/colors
+	echo "Move directory: $PWD"
+	echo "Run vim setup script..."
+	./setup.sh
+	cd $CURRENT_DIR
+	echo "Back dirrectory: $PWD"
+
+else
+	echo "Error!!! basic shell environments file is not exist"
+fi
--- vim/colors/setup.sh
+++ vim/colors/setup.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
-if [ ! -d ~/.vim ]; then
-mkdir ~/.vim
+BASIC_ENV=../../.setup/env/sh/setup_env
+
+source $BASIC_ENV
+
+VIM_DIR=$HOME/.vim
+if [ ! -d $VIM_DIR ]; then
+	mkdir $VIM_DIR
+	echo "mkdir $VIM_DIR"
 fi
 
 CURRENT_TIME=$(date +%Y)-$(date +%m)-$(date +%d)_$(date +%H)$(date +%M)$(date +%S)
-if [ -d ~/.vim/colors ]; then    # Directory is exist
+if [ -d $VIM_DIR/colors ]; then    # Directory is exist
 # Backup previous one
 echo "Directory is exist, backup to ~/.vim/color_bak_$CURRENT_TIME"
-mv ~/.vim/colors ~/.vim/colors_bak_$CURRENT_TIME
-elif [ -L ~/.vim/colors ]; then  # Symbolic Links is exist
+mv $VIM_DIR/colors $VIM_DIR/colors_bak_$CURRENT_TIME
+elif [ -L $VIM_DIR/colors ]; then  # Symbolic Links is exist
 # Backup previous one
 echo "Symbolic link is exist, backup to ~/.vim/color_bak_$CURRENT_TIME"
-mv ~/.vim/colors ~/.vim/colors_bak_$CURRENT_TIME
+mv $VIM_DIR/colors $VIM_DIR/colors_bak_$CURRENT_TIME
 else                            # No file and link
 echo "Not exist previous file and symbolic link"
 fi
-ln -fs  ~/DevEnv/vim/colors ~/.vim
+ln -fs  $ENV_HOME/vim/colors ~/.vim
